b42456bfd6c4253b0745e6255793f9a368ab6439
Abhilash <abhilashjha335@gmail.com>
2022-08-01 22:13:10 +0530

    Square of sorted array

---
+++ SquareOfSortedArray.cpp
@@ -0,0 +1,43 @@
+/*-----Leetcode question-------
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    vector<int> sortedSquares(vector<int>& nums){
+        vector<int> ans(nums.size());
+        int lft = 0;
+        int rgt = nums.size()-1;
+
+        for(int i = nums.size(); i>=0; i--){
+            if(abs(nums[lft])> abs(nums[rgt])){
+                ans[i] = nums[lft] * nums[lft];
+                lft++;
+            }
+            else{
+                ans[i] = nums[rgt] * nums[rgt];
+                rgt--;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+
+   return 0;
+}
